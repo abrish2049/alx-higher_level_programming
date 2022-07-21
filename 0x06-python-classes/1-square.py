b16d45d51561f87330bf61fa8950773f66a415b7
abrish2049 <abrahammenberu@gmail.com>
2022-07-21 07:42:43 +0000

    commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,13 +5,5 @@
 class Square:
 
     def __init__(self, size):
-        """__init__
-
-        The __init__ method initializes the size value
-        of the square.
-
-        Attributes:
-            size (int): The size of the square.
-
-        """
+        """__init__"""
         self.__size = size
---
+++ 0x06-python-classes/test/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This class don't do anything for the moment"""
+
+class Square:
+    """Class Square"""
+    pass
